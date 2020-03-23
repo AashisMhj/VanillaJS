1a8ab952d9fafe50d04d23495881d9cb17462229
Aashis2055 <9808034190a@gmail.com>
2020-03-23 16:30:49 +0545

    completed 4 exchange rate

---
+++ 4ExchangeRateCalculator/script.js
@@ -0,0 +1,44 @@
+ //making file request using fetch
+ function getData(){
+     fetch('items.json')
+        .then(res=>{
+            console.log(res)
+        })
+        .catch(res=>{
+            console.log(res);
+        })
+ }
+
+ const currencyEl_one  = document.getElementById('currency-one');
+ const currencyEl_two = document.getElementById('currency-two');
+ const amountEl_one  = document.getElementById('amount-one');
+ const amountEl_two = document.getElementById('amount-two');
+ const rateEl = document.getElementById('rate');
+ const swap = document.getElementById('swap');
+//fetch exchange reate
+ function calculate(){
+    const currencyOne = currencyEl_one.value;
+    const currencyTwo = currencyEl_two.value;
+
+    fetch(`https://api.exchangerate-api.com/v4/latest/${currencyOne}`)
+        .then(res=> res.json())
+        .then(data =>{
+            const rate = data.rates[currencyTwo];
+            rateEl.innerText = `1 ${currencyOne} = ${rate} ${currencyTwo}`;
+            amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+        })
+ }
+
+ //event listners
+ currencyEl_one.addEventListener('change',calculate);
+ amountEl_one.addEventListener('input',calculate);
+ currencyEl_two.addEventListener('change',calculate);
+ amountEl_two.addEventListener('input',calculate);
+
+ swap.addEventListener('click',()=>{
+     const temp = currencyEl_one.value;
+     currencyEl_one.value = currencyEl_two.value;
+     currencyEl_two.value = temp;
+     calculate();
+ })
+ calculate();
