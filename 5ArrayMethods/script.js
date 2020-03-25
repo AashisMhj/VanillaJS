923390102843f8fc5534a04fefd0fa31e1330858
Aashis2055 <9808034190a@gmail.com>
2020-03-25 08:09:35 +0545

    date
    completed 5 array methods

--- 5ArrayMethods/script.js
+++ 5ArrayMethods/script.js
@@ -1,8 +1,10 @@
 const main = document.getElementById('main');
 const addUserBtn = document.getElementById('add-user');
 const doubleBtn = document.getElementById('double');
-const showMillonairiesBtn = document.getElementById('show-millonaries');
+const showMillonairiesBtn = document.getElementById('show-millionaires');
 const sortBtn = document.getElementById('sort');
+const showMale = document.getElementById('show-male');
+const showFemale = document.getElementById('show-female');
 const calculateWealthBtn = document.getElementById('calculate-wealth');
 
 let data = [];
@@ -22,6 +24,33 @@ async function getRandomUser(){
     };
     console.log(newUser);
     addData(newUser);
+}
+//function to double the money of user
+function doubleMoney(){
+    data = data.map(user=>{
+        return {...user,money:user.money * 2};
+    })
+    updateDom();
+}
+//function to show only millonare
+//function to sort user by richest
+function sortByRichest(){
+    data.sort((a,b)=>{
+        return b.money - a.money;
+    })
+    updateDom();
+}
+function showMillonairies(){
+    data = data.filter((aUser)=> aUser.money > 1000000);
+    updateDom();
+}
+//function to calculate method
+function calculateWealth(){
+    // const total = array.reduce((acc,num) => (acc + num),0);//acc is the accumulated number and the last parameter is the starting number
+    const wealth = data.reduce((acc,aUser)=> (acc += aUser.money),0);
+    const wealthEl = document.createElement('div');
+    wealthEl.innerHTML = `<h3>Total wealth:<strong>${formatMoney(wealth)}</strong></h3>`;
+    main.appendChild(wealthEl);
 }
 //function to add fetched user to the data array
 getRandomUser();
@@ -43,9 +72,11 @@ function updateDom(providedDate = data){
 //simple function which adds $ sign and commas to the number and give the format of monay
 function formatMoney(number) {
     return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-  }
+}
 
   //event listners
-  addUserBtn.addEventListener('click',getRandomUser);
-  doubleBtn.addEventListener('click');
-  showMillonairiesBtn.addEventListener('click',)
+addUserBtn.addEventListener('click',getRandomUser);
+doubleBtn.addEventListener('click',doubleMoney);
+sort.addEventListener('click',sortByRichest);
+showMillonairiesBtn.addEventListener('click',showMillonairies);
+calculateWealthBtn.addEventListener('click',calculateWealth);
