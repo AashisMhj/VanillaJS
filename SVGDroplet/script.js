3c5ecc21630800806b9b59f09854b0938af479eb
Aashis2055 <9808034190a@gmail.com>
2020-03-29 09:16:40 +0545

    changes to svg droplet

--- SVGDroplet/script.js
+++ SVGDroplet/script.js
@@ -1,27 +1,31 @@
+//import dom elements
 const errorField = document.getElementById('error');
 const imageEl = document.getElementById('image');
-function allowDrop(ev) {
-    ev.preventDefault();
-  }
-  
-  function drag(ev) {
-      console.log("the drag function")
-    ev.dataTransfer.setData("text", ev.target.id);
-  }
-  
-  function drop(ev) {
-    ev.preventDefault();
-    console.log('the drop function');
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-  }
-
-  function onSVGDrop(event){
-      event.preventDefault();
-      name = event.dataTransfer.files[0].name;
-      if(!name.includes('.svg')){
-          errorField.innerText = "not a svg file";
-      }else{
-      imageEl.src = name;
-      }
-  }
+//this function will allow images and comonents to be droped in the div
+function allowDrop(event) {
+  event.preventDefault();
+}
+//function to enable drag   
+function drag(event) {
+  console.log("the drag function");
+  //on drag add data to the event
+  event.dataTransfer.setData("text", ev.target.id);
+}
+//function performed on drop  
+function drop(event) {
+  event.preventDefault();
+  console.log('the drop function');
+  let data = event.dataTransfer.getData("text");
+  event.target.appendChild(document.getElementById(data));
+}
+//function to show droped svg into div
+function onSVGDrop(event){
+  event.preventDefault();
+  name = event.dataTransfer.files[0].name;
+  if(!name.includes('.svg')){
+    errorField.innerText = "not a svg file";
+  }else{
+    imageEl.src = name;
+    errorField.innerText = "";
+    }
+}
