e72ddd4253acefedd1450690cf67bff8eb8f9a58
Aashis2055 <9808034190a@gmail.com>
2021-07-15 19:19:11 +0545

    DancingMen

--- DancingMen/script.js
+++ DancingMen/script.js
@@ -8,18 +8,22 @@ let select = document.getElementById('selected-image');
 //
 let encryptOut = document.getElementById('encrypt-output');
 let decryptOut = document.getElementById('decrypt-output')
-
+/**
+ * @description display all the images loooping though 65 t0 122 
+ */
 for(let i=65; i< 122; i++){
   if(i>90 && i<97)
     continue;
   btn = document.createElement('div');
   btn.classList = "btn"
+  btn.id = i;
   btn.addEventListener('click',(event)=>{
     let img = document.createElement('img');
+    console.log(event.target);
     img.src = `./images/char(${event.target.id}).png`;
     img.id = event.target.id;
     select.appendChild(img);
-  })
+  });
   btn.innerHTML = `<img  id="${i}" src="./images/char(${i}).png" /><p id="${i}">${String.fromCharCode(i)}</p>`;
   imagesDiv.appendChild(btn);
 }
@@ -27,6 +31,7 @@ for(let i=65; i< 122; i++){
 // event Handlers
 encryptBtn.addEventListener('click',(event)=>{
   let message = encryptMsg.value;
+  encryptOut.innerText = "";
   if(message.length === 0){
     encryptOut.innerText = "nothing to encrypt"
   }
